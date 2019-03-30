27cf7be43b1e18ef6a94bbeabff7c5b5f3a17818
emlabelle <citizen.jane.141@gmail.com>
2019-03-30 13:02:01 -0600

    postList component

--- src/components/post.js
+++ src/components/post.js
@@ -1,23 +1,17 @@
 import React from "react"
-import { comment } from "postcss-selector-parser"
 
 class Post extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      comments,
-    }
-  }
-  componentDidMount() {
-    //load post text
   }
   //onclick show modal
   render() {
-    const { postContent, commentCount } = this.props
+    const { postTitle, postContent, commentCount } = this.props
     return (
       <div onClick={showModal}>
-        {postContent}
-        <p>{commentCount} comments</p>
+        <h2>{postTitle}</h2>
+        <p>{postContent}</p>
+        <p>There are {commentCount} comments</p>
       </div>
     )
   }
