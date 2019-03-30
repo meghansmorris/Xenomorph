f221a06d4edbd68b5261b15ee68c9d90162906ca
Jared Gilpin <jared.gilpin@regiscompany.com>
2019-03-30 12:18:56 -0600

    layout udpated

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,21 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { } from "styled-components"
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: "none",
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
+
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
@@ -20,14 +25,16 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `#28587B`,
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
       </h1>
+      <button>add post</button>
     </div>
+    <hr />
   </header>
 )
 
